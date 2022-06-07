4dcc504b02625fd9a01a9ddc2460d4e9c29c0e80
shreegowri <gaganandh@gmail.com>
2022-06-08 03:26:19 +0530

    [shreegowri]Adding the parttime job

--- CEmplyeeWageProblem/CEmplyeeWageProblem/EmployeWageProblelm.cs
+++ CEmplyeeWageProblem/CEmplyeeWageProblem/EmployeWageProblelm.cs
@@ -7,28 +7,47 @@ using System.Threading.Tasks;
 namespace CEmplyeeWageProblem
 {
     internal class EmployeWageProblelm
+    { 
+    int IS_EmpPresent = 1;
+    int Wage_Per_Hour = 20;
+    int FullDay_Hour = 8;
+    int PartTimeDay_Hour = 4;
+    int DailyWage = 0;
+    int Full_Time = 1;
+    public void DailyEmpWgPartTime()
     {
-        int IS_EmpPresent = 1;
-        int Wage_Per_Hour = 20;
-        int FullDay_Hour = 8;
-        int DailyWage = 0;
-        public void DailyEmpWg()
+        Random ChechEmp = new Random();
+        int value = ChechEmp.Next(0, 2);
+        if (value == IS_EmpPresent)
         {
-            Random ChechEmp = new Random();
-            int value = ChechEmp.Next(0, 2);
-            if (value == IS_EmpPresent)
+            Random TimeChech = new Random();
+            int DayCheck = TimeChech.Next(0, 2);
+            if (DayCheck == Full_Time)
             {
+
                 DailyWage = FullDay_Hour * Wage_Per_Hour;
 
                 Console.WriteLine("Employee Present" + DailyWage);
-
             }
             else
             {
-                Console.WriteLine("Employee Absent");
+                DailyWage = PartTimeDay_Hour * Wage_Per_Hour;
 
+                Console.WriteLine("Employee Present for partTime" + DailyWage);
             }
 
+
+
+
+
+
         }
+        else
+        {
+            Console.WriteLine("Employee Absent");
+
+        }
+
     }
+}
 }
