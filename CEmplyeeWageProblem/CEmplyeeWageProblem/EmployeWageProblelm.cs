5f1fcc64d26e8582bc0d39b400b4c6a931745a24
shreegowri <gaganandh@gmail.com>
2022-06-08 03:17:00 +0530

    [shreegowri]Employee preesnt or Absent

---
+++ CEmplyeeWageProblem/CEmplyeeWageProblem/EmployeWageProblelm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEmplyeeWageProblem
+{
+    internal class EmployeWageProblelm
+    {
+        public int Is_EmpPresent = 1;
+
+        public void CheckEmpPresentAbsent()
+        {
+            Random ChechEmp = new Random();
+            int value = ChechEmp.Next(0, 2);
+            if (value == Is_EmpPresent)
+            {
+                Console.WriteLine("Employee Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee Absent");
+
+            }
+
+        }
+    }
+}
