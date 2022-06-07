763ddfe946fa125e0f6cb9a92ecb2f2bc270f4e1
shreegowri <gaganandh@gmail.com>
2022-06-08 03:39:40 +0530

    [shreegowri]calculating the total wage per month of the employee

--- CEmplyeeWageProblem/CEmplyeeWageProblem/EmployeWageProblelm.cs
+++ CEmplyeeWageProblem/CEmplyeeWageProblem/EmployeWageProblelm.cs
@@ -8,52 +8,60 @@ namespace CEmplyeeWageProblem
 {
     internal class EmployeWageProblelm
     {
-        public const float EmpWagePerHour = 20;
-
-        internal void CheckEmpPresentAbsent() { }
-
-
-        public const int FullTime_Working_Hours_PerDay = 8;
-        public const int PartTime_Working_Hours_PerDay = 4;
+        public const float EmpWagesPerHour = 20;
+        public const int FullTime_WorkingHrs_PerDay = 8;
+        public const int PartTime_WorkingHrs_PerDay = 4;
+        public const int MAX_WORKING_HRS = 100;
+        public const int MAX_WORKING_DAYS = 20;
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int IS_ABSENT = 0;
-        public const int Num_WorkingDays_Per_Month = 20;
-        public float EmpMonthlyWage = 0;
+        public float TotalWage = 0;
         float EmpDailyWage = 0;
 
+
+        //c# object-oriented class -object
+        //checking functionality write a metod
         public int IsEmployeePresent()
         {
             return new Random().Next(0, 3);
         }
 
         public void CalculateEmpWage()
+
         {
             int DayNumber = 1;
-            int EmpWorkingHours = 0;
-            while (DayNumber <= Num_WorkingDays_Per_Month)
-            {
+            int EmpWorkingHrs = 0;
+            int TotalWorkingHrs = 0;
 
+            //Random EmpCheck = new Random();//Random=class EmpCheck=object
+            //int Value = EmpCheck.Next(0, 3); //next=method .next it is a method  returns a non-negative integer
+
+            //for (int daymunber = 1; daymunber)
+            while (DayNumber <= MAX_WORKING_DAYS && TotalWorkingHrs <= MAX_WORKING_HRS)
+            {
                 switch (IsEmployeePresent())
+
                 {
                     case IS_ABSENT:
-                        EmpWorkingHours = 0;
+                        EmpWorkingHrs = 0;
                         break;
 
                     case IS_PART_TIME:
-                        EmpWorkingHours = PartTime_Working_Hours_PerDay;
+                        EmpWorkingHrs = PartTime_WorkingHrs_PerDay;
                         break;
-
                     case IS_FULL_TIME:
-                        EmpWorkingHours = FullTime_Working_Hours_PerDay;
+                        EmpWorkingHrs = FullTime_WorkingHrs_PerDay;
                         break;
                 }
-                EmpDailyWage = EmpWorkingHours * EmpWagePerHour;
+                EmpDailyWage = EmpWorkingHrs * EmpWagesPerHour;
+                TotalWage += EmpDailyWage;
 
-                EmpMonthlyWage += EmpDailyWage;
                 DayNumber++;
+                TotalWorkingHrs += EmpWorkingHrs;
             }
-            Console.WriteLine("Employee Monthly Wage:" + EmpMonthlyWage);
+            Console.WriteLine("Total working days :" + (DayNumber) + "\nTotal working hours:" + TotalWorkingHrs + "");
+
         }
     }
 }
