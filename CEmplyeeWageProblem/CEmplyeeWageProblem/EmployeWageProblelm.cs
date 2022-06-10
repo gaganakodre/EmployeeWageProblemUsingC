af53ab66905495cf2779e9e790d6586859d03582
shreegowri <gaganandh@gmail.com>
2022-06-10 23:16:26 +0530

    [shreegowri]Employee wage for multiple companies

--- CEmplyeeWageProblem/CEmplyeeWageProblem/EmployeWageProblelm.cs
+++ CEmplyeeWageProblem/CEmplyeeWageProblem/EmployeWageProblelm.cs
@@ -6,62 +6,64 @@ using System.Threading.Tasks;
 
 namespace CEmplyeeWageProblem
 {
-    internal class EmployeWageProblelm
+    class EmployeeWageComputation
     {
-        public const float EmpWagesPerHour = 20;
-        public const int FullTime_WorkingHrs_PerDay = 8;
-        public const int PartTime_WorkingHrs_PerDay = 4;
-        public const int MAX_WORKING_HRS = 100;
-        public const int MAX_WORKING_DAYS = 20;
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int IS_ABSENT = 0;
-        public float TotalWage = 0;
         float EmpDailyWage = 0;
+        public float TotalWage = 0;
+        private Dictionary<String, Company> Companies = new Dictionary<String, Company>();
 
+        public void AddCompany(String CompanyName, int EmpWagePerhour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHours_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
+        {
+            Company company = new Company(CompanyName.ToLower(), EmpWagePerhour, FullTime_WorkingHrs_PerDay, PartTime_WorkingHours_PerDay, MAX_WORKING_HRS, MAX_WORKING_DAYS);
+            Companies.Add(CompanyName.ToLower(), company);
+        }
 
-        //c# object-oriented class -object
-        //checking functionality write a metod
         public int IsEmployeePresent()
         {
             return new Random().Next(0, 3);
         }
 
-        public void CalculateEmpWage()
-
+        public void CalculateEmpWage(string CompanyName)
         {
             int DayNumber = 1;
-            int EmpWorkingHrs = 0;
+            int EmpWorkingHours = 0;
             int TotalWorkingHrs = 0;
 
-            //Random EmpCheck = new Random();//Random=class EmpCheck=object
-            //int Value = EmpCheck.Next(0, 3); //next=method .next it is a method  returns a non-negative integer
+            if (!Companies.ContainsKey(CompanyName.ToLower()))
+                throw new ArgumentNullException("Company Don't Exist");
+            Companies.TryGetValue(CompanyName.ToLower(), out Company company);
 
-            //for (int daymunber = 1; daymunber)
-            while (DayNumber <= MAX_WORKING_DAYS && TotalWorkingHrs <= MAX_WORKING_HRS)
+
+
+            while (DayNumber <= company.MAX_WORKING_DAYS && TotalWorkingHrs <= company.MAX_WORKING_HRS)
             {
-                switch (IsEmployeePresent())
 
+                switch (IsEmployeePresent())
                 {
                     case IS_ABSENT:
-                        EmpWorkingHrs = 0;
+                        EmpWorkingHours = 0;
                         break;
 
                     case IS_PART_TIME:
-                        EmpWorkingHrs = PartTime_WorkingHrs_PerDay;
+                        EmpWorkingHours = company.PartTime_WorkingHours_PerDay;
                         break;
+
                     case IS_FULL_TIME:
-                        EmpWorkingHrs = FullTime_WorkingHrs_PerDay;
+                        EmpWorkingHours = company.FullTime_WorkingHrs_PerDay;
                         break;
                 }
-                EmpDailyWage = EmpWorkingHrs * EmpWagesPerHour;
-                TotalWage += EmpDailyWage;
+                EmpDailyWage = EmpWorkingHours * company.EmpWagePerHour;
 
+                TotalWage += EmpDailyWage;
                 DayNumber++;
-                TotalWorkingHrs += EmpWorkingHrs;
+                TotalWorkingHrs += EmpWorkingHours;
             }
-            Console.WriteLine("Total working days :" + (DayNumber) + "\nTotal working hours:" + TotalWorkingHrs + "");
+            Console.WriteLine("Total working days :" + (DayNumber - 1) + "\n Total working hours :" + TotalWorkingHrs + "");
 
         }
+
     }
 }
--- CEmplyeeWageProblem/CEmplyeeWageProblem/Program.cs
+++ CEmplyeeWageProblem/CEmplyeeWageProblem/Program.cs
@@ -1,13 +1,28 @@
 ﻿namespace CEmplyeeWageProblem
 
 {
-    class program
+    class Program
     {
-        public static void Main(String[] args)
+        static void Main(string[] args)
         {
 
-            Console.WriteLine("Welocome to the EmployeeWage Calculation");
+            Console.WriteLine("1 For TATA Company Employee Wage");
+            Console.WriteLine("2 For MAHINDRA Company Employee Wage");
 
+            int problem = Convert.ToInt32(Console.ReadLine());
+            switch (problem)
+            {
+                case 1:
+                    EmployeeWageComputation employeewagecomputation = new EmployeeWageComputation();
+                    employeewagecomputation.AddCompany("TATA", 20, 8, 4, 100, 20);
+                    employeewagecomputation.CalculateEmpWage("TATA");
+                    break;
+                case 2:
+                    EmployeeWageComputation wagecomputation = new EmployeeWageComputation();
+                    wagecomputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
+                    wagecomputation.CalculateEmpWage("MAHINDRA");
+                    break;
+            }
         }
     }
 
